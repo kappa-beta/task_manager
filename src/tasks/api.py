7774cf6d329cca64bbbfb22c9445c67f290c6870
GKV <gkv@tz74.ru>
2022-02-09 16:55:01 +0500

    "backup"

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, APIRouter, status, HTTPException
 
-from src.exceptions import EntityConflictError
-from src.tasks.schemas import Task as TaskSchema, TaskCreate
+from src.exceptions import EntityConflictError, EntityDoesNotExistError
+from src.tasks.schemas import Task as TaskSchema, TaskCreate, TaskUpdate
 from src.tasks.services import TaskService
 
 router = APIRouter(
@@ -27,3 +29,37 @@ def create_task(
     except EntityConflictError:
         raise HTTPException(status.HTTP_409_CONFLICT) from None
     return task
+
+
+@router.get('/{task_id}', response_model=TaskSchema)
+def get_task(
+        task_id: int,
+        service: TaskService = Depends(),
+):
+    try:
+        return service.get_task(task_id)
+    except EntityDoesNotExistError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND) from None
+
+
+@router.get('', response_model=List[TaskSchema])
+def get_tasks(
+        service: TaskService = Depends(),
+):
+    try:
+        return service.get_tasks()
+    except EntityDoesNotExistError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND) from None
+
+
+@router.patch('/{task_id}', response_model=TaskSchema)
+def edit_task(
+        task_id: int,
+        task_update: TaskUpdate,
+        service: TaskService = Depends(),
+):
+    try:
+        task = service.update_task(task_id, task_update)
+        return task
+    except EntityDoesNotExistError:
+        raise HTTPException(status.HTTP_404_NOT_FOUND) from None
--- src/tasks/services.py
+++ src/tasks/services.py
@@ -1,11 +1,12 @@
 from fastapi import Depends
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 from src.config import get_settings
 from src.database import get_session
-from src.exceptions import EntityConflictError
+from src.exceptions import EntityConflictError, EntityDoesNotExistError
 from src.tasks.models import Task
-from src.tasks.schemas import TaskCreate
+from src.tasks.schemas import TaskCreate, TaskUpdate
 
 
 class TaskService:
@@ -27,3 +28,30 @@ class TaskService:
             return task
         except IntegrityError:
             raise EntityConflictError from None
+
+    def get_task(self, task_id: int) -> Task:
+        return self._get_task(task_id)
+
+    def get_tasks(self) -> Task:
+        tasks = self.session.execute(
+            select(Task)
+        ).scalars().all()
+        return tasks
+
+    def update_task(self, task_id: int, task_update: TaskUpdate):
+        task = self._get_task(task_id)
+        for k, v in task_update.dict(exclude_unset=True):
+            setattr(task, k, v)
+
+        self.session.commit()
+        return task
+
+    def _get_task(self, task_id: int) -> Task:
+        try:
+            task = self.session.execute(
+                select(Task)
+                    .where(Task.id == task_id)
+            ).scalar_one()
+            return task
+        except NoResultFound:
+            raise EntityDoesNotExistError from None
