0f4a1f51106b32a276f0074f5ef52804b24db87b
GKV <gkv@tz74.ru>
2022-07-11 17:15:08 +0500

    backup

---
+++ static/post.js
@@ -0,0 +1,24 @@
+formElem.onsubmit = async (e) => {
+	e.preventDefault();
+
+	let test = new FormData(formElem)
+
+	var object = {};
+		test.forEach(function(value, key){
+		object[key] = value;
+	});
+	var json = JSON.stringify(object);
+	console.log(json);
+
+	let response = await fetch('/users/', {
+	method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: json
+	});
+
+	let result = await response.json();
+	console.log(result);
+};
